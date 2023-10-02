c8913e7664897878b803d8396f0e1efc1d159b86
projekbrillylutfan <projekbrillylutfan@gmail.com>
2023-10-02 20:09:05 +0700

    js dasar

---
+++ index.js
@@ -0,0 +1,10 @@
+//buat fuction untuk pertambahan 2 angka
+function coba_tambah() {
+  const x = 21;
+  const y = 23;
+
+  const z_jumlah = x + y;
+  return z_jumlah;
+}
+
+console.log(coba_tambah());
