5475b4cfd249a42d301ed3402279cac411246681
projekbrillylutfan <projekbrillylutfan@gmail.com>
2023-10-03 20:57:47 +0700

    penambahan kodingan Assignment operator

---
+++ Operator_Expression.js
@@ -0,0 +1,20 @@
+// Assignment operator
+// let x = 10;
+
+// x += 80;
+
+// console.log("x : ", x)
+
+// object 
+let person = {name : "sabrina", age : 21};
+
+// array 
+let fruits = ["mangga", "apel", "jeruk"];
+
+// assisgnment terhadap atribut object 
+person.name = "agus";
+console.log("person ", person)
+
+// assisgnment terhadap item di dalam array
+fruits[0] = "semangka"
+console.log("fruits ", fruits)
